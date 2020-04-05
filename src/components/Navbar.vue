41ca427cea3c39d50ca88f837d5581821ba019fb
47karimasif <karimasif98@gmail.com>
2020-04-05 19:29:03 +0530

    updated

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,15 +1,27 @@
 <template>
   <nav>
     <v-app-bar app flat>
-      <v-app-bar-nav-icon
-        class="grey--text"
-        @click="drawer = !drawer"
-      ></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon class="grey--text" @click="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title class="text-uppercase grey-text">
         <span class="font-weight-light">Todo</span>
         <span>47</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
+
+      <v-menu offset-y :elevation="0">
+        <template v-slot:activator="{ on }">
+          <v-btn text v-on="on" class="grey--text">
+            <v-icon left>fas fa-chevron-down</v-icon>
+            <span>Menu</span>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item v-for="(link, index) in links" :key="index" router :to="link.route">
+            <v-list-item-title>{{ link.text }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+
       <v-btn text>
         <span>Sign-Out</span>
         <v-icon right>mdi-exit-to-app</v-icon>
@@ -28,19 +40,16 @@
         </v-list-item>
       </v-list>
       <v-list>
-        <v-list-item
-          v-for="link in links"
-          :key="link.text"
-          router
-          :to="link.route"
-        >
+        <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
           <v-list-item-icon>
             <v-icon class="white--text">{{ link.icon }}</v-icon>
           </v-list-item-icon>
           <v-list-item-content>
-            <v-list-item-title class="white--text">{{
+            <v-list-item-title class="white--text">
+              {{
               link.text
-            }}</v-list-item-title>
+              }}
+            </v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -2,7 +2,21 @@
   <div class="projects">
     <h1 class="subheading grey--text text-center">This is Projectpage</h1>
     <v-container class="my-5">
-      content
+      <v-expansion-panels>
+        <v-expansion-panel v-for="project in projects" :key="project.title" class="grey--text">
+          <v-expansion-panel-header class="d-flex flex-column">
+            <div>{{project.title}}</div>
+            <v-card flat class="mt-5">
+              <div class="font-weight-bold grey--text">due by {{project.due}}</div>
+            </v-card>
+          </v-expansion-panel-header>
+          <v-expansion-panel-content>
+            <v-card flat>
+              <div>{{project.content}}</div>
+            </v-card>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
     </v-container>
   </div>
 </template>
@@ -10,6 +24,45 @@
 <script>
 export default {
   name: "projects",
-  components: {}
+  components: {},
+  setup() {
+    const projects = [
+      {
+        title: "Design a new website",
+        person: "The Net Ninja",
+        due: "1st Jan 2019",
+        status: "ongoing",
+        content:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!"
+      },
+      {
+        title: "Code up the homepage",
+        person: "Chun Li",
+        due: "10th Jan 2019",
+        status: "complete",
+        content:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!"
+      },
+      {
+        title: "Design video thumbnails",
+        person: "Ryu",
+        due: "20th Dec 2018",
+        status: "complete",
+        content:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!"
+      },
+      {
+        title: "Create a community forum",
+        person: "Gouken",
+        due: "20th Oct 2018",
+        status: "overdue",
+        content:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!"
+      }
+    ];
+    return {
+      projects
+    };
+  }
 };
 </script>
